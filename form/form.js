4fce633818fb73ea86cc9a157a1c18aab4ee5b87
Ian <ianguimaraes31@gmail.com>
2020-11-12 22:35:31 -0300

    Enviando as informacoes do formulario para o servidor

--- form/form.js
+++ form/form.js
@@ -1,1 +1,34 @@
-console.log("Hey");
+function getQuerystring(search) {
+  var pairs = location.search.slice(1).split('&');
+  var result = {};
+
+  pairs.forEach(function(pair) {
+      pair = pair.split('=');
+      result[pair[0]] = decodeURIComponent(pair[1] || '');
+  });
+
+  return result;
+}
+
+function handleFormSubmit () {
+  const interest = document.getElementById("selecInte").value;
+  const quality = document.getElementById("selecQuali").value;
+  const review = document.getElementById("relatoExp").value;
+  const { idStudent, idClass } = getQuerystring(window.location.search);
+  
+  console.log(interest, quality, review, idStudent, idClass);
+
+  let response = fetch('/student', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify({ interest, quality, review, idStudent, idClass })
+  })
+  .then(response => response.json())
+  .then(json =>  {
+    console.log(json);
+  })
+  .catch(error => console.log(error));
+}
+
